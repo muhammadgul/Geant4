42af142dab243129dbcacd5322ed3a1bc9046832
muhammadgul <mgul@cern.ch>
2019-07-02 15:44:24 +0200

    Update garfield.sh
    Updating gcc 4.8 > 4.9

--- garfield.sh
+++ garfield.sh
@@ -8,6 +8,14 @@ export GARFIELD_HOME=/home/muhammad/work/Tools/garfield
 # Dowlaoad the source code
 git clone ssh://git@gitlab.cern.ch:7999/garfield/garfieldpp.git $GARFIELD_HOME
 
+# upgrade gcc 4.8 to 4.9 using info from:
+https://wiki.audacityteam.org/wiki/Upgrade_to_gcc_4.9
+
+#  Using the first method, just type
+make
+
+#Following for the second method
+-------------------------------
 # Building the library
 # make build directory
 mkdir $GARFIELD_HOME/build; cd $GARFIELD_HOME/build
@@ -22,3 +30,5 @@ cmake -DCMAKE_BUILD_TYPE=Debug $GARFIELD_HOME
 make
 make install
 rm -rf build
+-------------------------------------------
+
